0628f47c896e33f23832718f2c0eed79fc078c8b
Brian Besaw <bjbesaw@outlook.com>
2017-08-29 16:28:28 -0400

    cell listener tested sat

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
+'use strict';
+
 const appState = {
+  lastMarkedCell: null,
+  currBoard: [null,null,null,null,null,null,null,null,null],
+  currCell: null,
+  currMarker: 'X',
+  winCombo: null,   //element in winning combo array
+  winCombos: [
+    [0,1,2], [3,4,5], [6,7,8], 
+    [0,3,6], [1,4,7], [2,5,8], 
+    [0,4,8], [2,4,6] 
+  ]
 };
 
-// State modification functions
+let currCellID;
 
-// Render functions
+function cellHandler(){
+  $('.board').on('click', '.cell', event => {
+    //console.log('logging ' + $(event.currentTarget).attr('id'));
+    if($(event.currentTarget).find('p').text().trim() === ''){
+      console.log('running update board');
+      currCellID = parseInt($(event.currentTarget).attr('id'));
+      appState.currBoard[currCellID] = appState.currMarker;
+    }
+    console.log(appState.currBoard);
+  });
+}
 
-// Event Listeners
+
+function btnHandler(){
+  $('#new-game').on('click',  event => {
+    console.log('new-game ran');
+    
+  });
+
+}
+
+function handleTTTApp(){
+  cellHandler();
+  btnHandler();
+}
+
+$(handleTTTApp);
